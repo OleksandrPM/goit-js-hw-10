286b58bac55a3d8067a2c4ed337dc824e41790f6
Oleksandr Moskaliuk <olekspm@gmail.com>
2023-03-19 19:13:08 +0100

    Added task`s conditions

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+const baseUrl = 'https://restcountries.com/v3.1/all';
+
+function fetchCountries(name) {
+  return fetch(baseUrl, options);
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
 
+import debounce from 'lodash.debounce';
+
 const DEBOUNCE_DELAY = 300;
+const toMuchMessage =
+  'Too many matches found. Please enter a more specific name.';
+const notFoundMessage = '"Oops, there is no country with that name"';
